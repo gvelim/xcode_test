702250068c85ea40f25fafb42a05ea16b365ed3c
george <github@velimachitis.me>
2021-04-09 17:41:18 +0100

    .

---
+++ liner/liner/liner/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  liner
+//
+//  Created by George  on 09/04/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i;
+    
+    for (i=0; i<10; i++) {
+        printf("Hello, World!\n");
+    }
+    return i;
+}
