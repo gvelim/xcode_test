b65876ed7da83bea097f53543bfb71ecd1ef05c1
george <github@velimachitis.me>
2021-04-09 20:06:28 +0100

    ..

--- liner/liner/liner/main.c
+++ liner/liner/liner/main.c
@@ -8,29 +8,36 @@
 #include <stdio.h>
 
 // forward declarations
-typedef struct AnObject sObj;   // Object type
-int add(sObj* this);            // Object method
-void initAnObject(sObj* so);    // Object constructor
+typedef struct AnObject sObj;   	// Object type
+void set(sObj* this, int a, int b);
+int add(sObj* this);            	// Object method
+void initAnObject(sObj* so);    	// Object constructor
 
 // Type declarations
 typedef struct AnObject {
     int a;
     int b;
+	void (*set)(sObj*, int, int);
     int (*add)(sObj* this);
 } sObj;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     int i;
-    sObj aObj,bObj;
+    sObj aObj,bObj,cObj;
     
     initAnObject(&aObj);
     initAnObject(&bObj);
-    
-    for (i=0; i<10; i++) {
+	initAnObject(&cObj);
+	
+	aObj.set(&aObj, 10, 10);
+	bObj.set(&bObj, 1, 1);
+	cObj.set(&cObj, 5, 1);
+	
+   for (i=0; i<10; i++) {
         aObj.a += i;
         bObj.b += aObj.a;
-        printf("Hello.. aObj.a:%d, bObj.b: %d\n", aObj.add(&aObj), bObj.add(&bObj) );
+        printf("Hello.. aObj:%d, bObj: %d, cObj: %d\n", aObj.add(&aObj), bObj.add(&bObj), cObj.add(&cObj) );
     }
     return i;
 }
@@ -42,11 +49,16 @@ int main(int argc, const char * argv[]) {
 
 void initAnObject(sObj* so) {
     so->add = &add;
-    so->a = 10;
-    so->b = 1;
+	so->set = &set;
+    so->set(so, 10, 1);
     return;
 }
 
 int add(sObj* this) {
     return this->a + this->b;
+}
+
+void set(sObj* this, int a, int b) {
+	this->a = a;
+	this->b = b;
 }
