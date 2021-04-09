08542b3b1914a70ff37c15e9f6fd7b3aafb0df95
george <github@velimachitis.me>
2021-04-09 17:53:37 +0100

    OOD in C

--- liner/liner/liner/main.c
+++ liner/liner/liner/main.c
@@ -7,12 +7,28 @@
 
 #include <stdio.h>
 
+typedef struct objectOrientedDummy {
+    int a;
+    int b;
+    int (*add)(void* this);
+} ood;
+
+int add(ood* this) {
+    return this->a + this->b;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     int i;
+    ood dummy;
+    
+    dummy.add = (void*)&add;
+    dummy.a = 10;
+    dummy.b = 1;
     
     for (i=0; i<10; i++) {
-        printf("Hello, World!\n");
+        dummy.a += i;
+        printf("Hello, %d World!\n", dummy.add(&dummy) );
     }
     return i;
 }
